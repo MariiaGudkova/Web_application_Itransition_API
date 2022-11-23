4f8df436397edee4682e69e23b97e46dd6d13afa
Gudkova Mariia <gudmary8@gmail.com>
2022-11-23 23:36:57 +0400

    testing cors

--- middlewares/handleCors.js
+++ middlewares/handleCors.js
@@ -4,9 +4,10 @@ function handleCors(req, res, next) {
   const { method } = req;
   const { origin } = req.headers;
   const requestHeaders = req.headers["access-control-request-headers"];
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
+  // if (allowedCors.includes(origin)) {
+  //   res.header("Access-Control-Allow-Origin", origin);
+  // }
+  res.header("Access-Control-Allow-Origin", "*");
   if (method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
     res.header("Access-Control-Allow-Headers", requestHeaders);
